0cef19db7e598cc0a2cf678857011620090b9c21
mubadara <renaud.bineau@gmail.com>
2020-06-21 11:51:13 +0200

    code add

--- public/article-new.php
+++ public/article-new.php
@@ -31,7 +31,15 @@ if ($_POST) {
     if (isset($_POST['name'])) {
         $article['name'] = $_POST['name'];
     }
-    
+    if (isset($_POST['description'])) {
+        $article['description'] = $_POST['description'];
+    }
+    if (isset($_POST['price'])) {
+        $article['price'] = $_POST['price'];
+    }
+    if (isset($_POST['quantity'])) {
+        $article['quantity'] = $_POST['quantity'];
+    }
     if (!isset($_POST['name'])|| empty($_POST['name'])) {
         $errors['name'] = true;
         $messages['name'] = 'Merci de renseigner le nom de l\'article';
@@ -40,6 +48,34 @@ if ($_POST) {
         $errors['name'] = true;
         $messages['name'] = 'Le nombre de caractères doit être inclu en 2 et 100 pour le champ nom';
     }
+    if (isset($_POST['description']) && strpos($_POST['description'], '<')
+    || strpos($_POST['description'], '>')) {
+        $errors = true;
+        $messages = 'la description contient un caractère interdit < ou >';
+    }
+    if (!isset($_POST['price'])|| empty($_POST['price'])) {
+        $errors['price'] = true;
+        $messages['price'] = 'Merci de renseigner le prix de l\'article';
+    }
+    elseif(!is_numeric($_POST['price'])){
+        $errors['price'] = true;
+        $messages['price'] = "Le prix doit être une valeur numérique";
+    }
+    if (!isset($_POST['quantity'])|| empty($_POST['quantity'])) {
+        $errors['quantity'] = true;
+        $messages['quantity'] = 'Merci de renseigner la quantité';
+    }
+    elseif(!is_numeric($_POST['price']) || is_float($_POST['quantity']) || ($_POST['price']) < 0){
+        $errors['price'] = true;
+        $messages['price'] = "La quantité doit être un nombre entier";
+    }
+    if (!$errors){
+        //Le code suivant permet de rediriger vers la page qui liste les articles existants :
+    $url = 'articles.php';
+    header("Location: {$url}", true, 302);
+    exit();
+    }
+        
 }
 
 
